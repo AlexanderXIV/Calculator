b6e5739f2c3bd2fa0d20ea2143e466cdb3c2e6f9
Alexander <50303374+AlexanderXIV@users.noreply.github.com>
2020-07-13 15:21:25 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,3 +1,4 @@
+
 /**
  * @date 4.7.2020
  * 
@@ -13,6 +14,7 @@
  */
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 // Tokenizer:
 // ==========================================
@@ -26,25 +28,6 @@ class TokenValue extends Token {
   public TokenValue(double value) {
     this.value = value;
   }
-
-  @Override
-  public String toString() {
-    return Double.toString(value);
-  }
-}
-
-class TokenOpen extends Token {
-  @Override
-  public String toString() {
-    return "(";
-  }
-}
-
-class TokenClose extends Token {
-  @Override
-  public String toString() {
-    return ")";
-  }
 }
 
 class TokenOperator extends Token {
@@ -53,29 +36,64 @@ class TokenOperator extends Token {
   public TokenOperator(char operator) {
     this.operator = operator;
   }
+}
 
-  @Override
-  public String toString() {
-    return Character.toString(operator);
+class TokenOpen extends Token {
+}
+
+class TokenClose extends Token {
+}
+
+class TokenFunction extends Token {
+  String str;
+
+  public TokenFunction(String str) {
+    this.str = str;
   }
 }
 
 class Tokenizer {
+  public static boolean isDouble(String str) {
+    if (str.length() == 0)
+      return false;
+    for (char c : str.toCharArray()) {
+      if ((Character.isDigit(c) || c == '.') == false) return false;
+    }
+    try {
+      Double.parseDouble(str);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
+  public static Token make_token(String str) {
+    if (str.equals("sin") || str.equals("cos") || str.equals("tan") || str.equals("asin") || str.equals("acos")
+        || str.equals("atan") || str.equals("exp") || str.equals("sqrt") || str.equals("abs"))
+      return new TokenFunction(str);
+    if (str.equals("e"))
+      return new TokenValue(Math.E);
+    if (str.equals("pi"))
+      return new TokenValue(Math.PI);
+    if (isDouble(str))
+      return new TokenValue(Double.parseDouble(str));
+    if (isDouble(str.substring(0, str.length() - 1)) && str.charAt(str.length() - 1) == 'd')
+      return new TokenValue(Math.toRadians(Double.parseDouble(str)));
+    System.out.println("Unexpected Token: " + str);
+    System.exit(1);
+    return null;
+  }
+
   public static ArrayList<Token> tokenize(String str) {
+    String cur = "";
     ArrayList<Token> tokens = new ArrayList<>();
 
-    String cur = "";
     for (int i = 0; i < str.length(); i++) {
       char chr = str.charAt(i);
-
       if (chr == '(' || chr == ')' || chr == '+' || chr == '-' || chr == '*' || chr == '/' || chr == '^'
           || chr == ' ') {
-        if (cur.length() != 0) {
-          if (tokens.size() > 0 && tokens.get(tokens.size() - 1).getClass() == TokenClose.class)
-            tokens.add(new TokenOperator('*'));
-          tokens.add(new TokenValue(Double.parseDouble(cur)));
-          if (tokens.size() > 0 && chr == '(')
-            tokens.add(new TokenOperator('*'));
+        if (cur.length() > 0) {
+          tokens.add(make_token(cur));
           cur = "";
         }
 
@@ -85,22 +103,27 @@ class Tokenizer {
           tokens.add(new TokenClose());
         } else if (chr == '-') {
           if (tokens.size() == 0)
-            cur += chr;
-          else
-            tokens.add(new TokenOperator(chr));
+            tokens.add(new TokenValue(0));
+          tokens.add(new TokenOperator(chr));
         } else if (chr != ' ') {
           tokens.add(new TokenOperator(chr));
         }
       } else {
-        cur += chr;
+        if (isDouble(cur) && isDouble(cur + chr) == false && chr == 'd') {
+          tokens.add(make_token(cur + chr));
+          cur = "";
+        } else {
+          if (isDouble(cur) && isDouble(cur + chr) == false) {
+            tokens.add(make_token(cur));
+            cur = "";
+          }
+          cur += chr;
+        }
       }
     }
 
-    if (cur.length() != 0) {
-      if (tokens.size() > 0 && tokens.get(tokens.size() - 1).getClass() == TokenClose.class)
-        tokens.add(new TokenOperator('*'));
-      tokens.add(new TokenValue(Double.parseDouble(cur)));
-    }
+    if (cur.length() > 0)
+      tokens.add(make_token(cur));
 
     return tokens;
   }
@@ -108,97 +131,11 @@ class Tokenizer {
 
 // Parser:
 // ==========================================
-
-abstract class Node {
-}
-
-class NodeValue extends Node {
-  double value;
-
-  public NodeValue(double value) {
-    this.value = value;
-  }
-
-  @Override
-  public String toString() {
-    return "(" + value + ")";
-  }
-}
-
-class NodeAdd extends Node {
-  Node a, b;
-
-  public NodeAdd(Node a, Node b) {
-    this.a = a;
-    this.b = b;
-  }
-
-  @Override
-  public String toString() {
-    return "(" + a + " + " + b + ")";
-  }
-}
-
-class NodeSub extends Node {
-  Node a, b;
-
-  public NodeSub(Node a, Node b) {
-    this.a = a;
-    this.b = b;
-  }
-
-  @Override
-  public String toString() {
-    return "(" + a + " - " + b + ")";
-  }
-}
-
-class NodeMul extends Node {
-  Node a, b;
-
-  public NodeMul(Node a, Node b) {
-    this.a = a;
-    this.b = b;
-  }
-
-  @Override
-  public String toString() {
-    return "(" + a + " * " + b + ")";
-  }
-}
-
-class NodeDiv extends Node {
-  Node a, b;
-
-  public NodeDiv(Node a, Node b) {
-    this.a = a;
-    this.b = b;
-  }
-
-  @Override
-  public String toString() {
-    return "(" + a + " / " + b + ")";
-  }
-}
-
-class NodePow extends Node {
-  Node a, b;
-
-  public NodePow(Node a, Node b) {
-    this.a = a;
-    this.b = b;
-  }
-
-  @Override
-  public String toString() {
-    return "(" + a + " ^ " + b + ")";
-  }
-}
-
 class Parser {
   ArrayList<Token> tokens;
   int cur;
 
+  // Potenz vor Punkt vor Strich
   public int OPERATORS(char chr) {
     if (chr == '+' || chr == '-') {
       return 1;
@@ -235,19 +172,39 @@ class Parser {
     cur--;
   }
 
-  public Node parse(int level) {
-    Node result = null;
+  public double parse(int level) {
+    double result = 0;
 
     if (take(TokenValue.class)) {
-      result = new NodeValue(((TokenValue) token()).value);
+      result = ((TokenValue) token()).value;
     } else if (take(TokenOpen.class)) {
       result = parse(0);
-      take(TokenClose.class);
-    }
+      if (!take(TokenClose.class)) {
+        System.out.println("Fehler: Klammern");
+        System.exit(1);
+      }
+    } else if (take(TokenFunction.class)) {
+      String str = ((TokenFunction) token()).str;
+      result = parse(0);
 
-    if (result == null) {
-      System.out.println("error");
-      return result;
+      if (str.equals("abs"))
+        result = Math.abs(result);
+      else if (str.equals("sqrt"))
+        result = Math.sqrt(result);
+      else if (str.equals("sin"))
+        result = Math.sin(result);
+      else if (str.equals("cos"))
+        result = Math.cos(result);
+      else if (str.equals("tan"))
+        result = Math.tan(result);
+      else if (str.equals("asin"))
+        result = Math.asin(result);
+      else if (str.equals("acos"))
+        result = Math.acos(result);
+      else if (str.equals("atan"))
+        result = Math.atan(result);
+      else if (str.equals("exp"))
+        result = Math.exp(result);
     }
 
     while (take(TokenOperator.class)) {
@@ -258,22 +215,18 @@ class Parser {
         return result;
       }
 
-      Node b = parse(OPERATORS(chr));
-      if (b == null) {
-        System.out.println("error");
-        return result;
-      }
+      double b = parse(OPERATORS(chr));
 
       if (chr == '+') {
-        result = new NodeAdd(result, b);
+        result = result + b;
       } else if (chr == '-') {
-        result = new NodeSub(result, b);
+        result = result - b;
       } else if (chr == '*') {
-        result = new NodeMul(result, b);
+        result = result * b;
       } else if (chr == '/') {
-        result = new NodeDiv(result, b);
+        result = result / b;
       } else if (chr == '^') {
-        result = new NodePow(result, b);
+        result = Math.pow(result, b);
       }
     }
 
@@ -282,30 +235,31 @@ class Parser {
 }
 
 class Calculator {
-  public static double calc(Node n) {
-    if (n.getClass() == NodeValue.class)
-      return ((NodeValue) n).value;
-    if (n.getClass() == NodeAdd.class)
-      return calc(((NodeAdd) n).a) + calc(((NodeAdd) n).b);
-    if (n.getClass() == NodeSub.class)
-      return calc(((NodeSub) n).a) - calc(((NodeSub) n).b);
-    if (n.getClass() == NodeMul.class)
-      return calc(((NodeMul) n).a) * calc(((NodeMul) n).b);
-    if (n.getClass() == NodeDiv.class)
-      return calc(((NodeDiv) n).a) / calc(((NodeDiv) n).b);
-    if (n.getClass() == NodePow.class)
-      return Math.pow(calc(((NodePow) n).a), calc(((NodePow) n).b));
-    return 0;
+  public static double calculate(String str) {
+    return new Parser(Tokenizer.tokenize(str.toLowerCase())).parse(0);
   }
 }
 
 public class Test {
   public static void main(String[] args) {
-    ArrayList<Token> tokens = Tokenizer.tokenize("-5(3 +6)2-3*2^5");
-    System.out.println("Tokens: " + tokens);
-    Node n = new Parser(tokens).parse(0);
-    System.out.println("Node: " + n);
-    double d = Calculator.calc(n);
-    System.out.println("Ergebniss: " + d);
+    Scanner eingabe = new Scanner(System.in);
+    String str;
+
+    System.out.println("Taschenrechner");
+    System.out
+        .println("Operationen: '+', '-', '*', '/', '^', 'sin', 'asin, 'cos', 'acos', 'tan', 'atan', 'exp', 'abs'");
+    System.out.println("Konstanten: 'e', 'pi'");
+    System.out.println(
+        "Hinweis: Alles wird in Bogenmaß gerechnet. Um in Grad zu rechnen hinter der gewünschten Zahl 'd' (degree) platzieren.");
+    System.out.println("Syntax: 'sin cos 20' entspricht 'sin(cos(20))'");
+
+    boolean b = true;
+    while (b) {
+      System.out.print("> ");
+      str = eingabe.nextLine();
+      System.out.println("Ergebnis: " + Calculator.calculate(str));
+    }
+
+    eingabe.close();
   }
 }
